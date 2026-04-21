23e7b9c4436d3b9d377525677707ecb892346087
DeepAgent <deepagent@abacus.ai>
2026-04-21 10:14:27 +0000

    Fix Prisma schema and next.config for Hostinger compatibility

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
+  distDir: '.next',
   productionBrowserSourceMaps: false,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -15,13 +9,6 @@ const nextConfig = {
     ignoreBuildErrors: false,
   },
   images: { unoptimized: true },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.filename = 'static/chunks/[name]-[contenthash:8].js';
-      config.output.chunkFilename = 'static/chunks/[contenthash:16].js';
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
